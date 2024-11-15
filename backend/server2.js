1abfadd3faf45799b517993fbf0231d19e31df0f
rajesh <rajesh.swain@innoitlabs.com>
2024-11-15 11:46:31 +0530

    changess

--- backend/server2.js
+++ backend/server2.js
@@ -9,30 +9,30 @@ const PORT = process.env.PORT || 8081;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Function to call Gemini API (GET request)
-const callGeminiAPI = async () => {
-    try {
-        const response = await axios.get('https://api.gemini.com/v1/pubticker/btcusd');
-        console.log(response.data)
-        return response.data;
-    } catch (error) {
-        console.error('Error calling Gemini API:', error.response ? error.response.data : error.message);
-        throw error;
-    }
-};
+// // Function to call Gemini API (GET request)
+// const callGeminiAPI = async () => {
+//     try {
+//         const response = await axios.get('https://api.gemini.com/v1/pubticker/btcusd');
+//         console.log(response.data)
+//         return response.data;
+//     } catch (error) {
+//         console.error('Error calling Gemini API:', error.response ? error.response.data : error.message);
+//         throw error;
+//     }
+// };
 
-// Endpoint to handle chat messages
-app.post('/api/chat', async (req, res) => {
-    const { inputChat } = req.body;  
-    console.log('Received inputChat:', inputChat);
+// // Endpoint to handle chat messages
+// app.post('/api/chat', async (req, res) => {
+//     const { inputChat } = req.body;  
+//     console.log('Received inputChat:', inputChat);
 
-    try {
-        const result = await callGeminiAPI(); 
-        res.json({ message: `Price: ${result.last} USD` });  
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch data from Gemini API' });
-    }
-});
+//     try {
+//         const result = await callGeminiAPI(); 
+//         res.json({ message: `Price: ${result.last} USD` });  
+//     } catch (error) {
+//         res.status(500).json({ error: 'Failed to fetch data from Gemini API' });
+//     }
+// });
 app.get('/', (req, res) => {
     res.send("hi rajesh how r u");
 });
